b44d32b1c94e284f2541bde9f7f0f53d47e0dc11
Peter Heilbo Ratgen <peterratgen@gmail.com>
2019-05-08 00:14:03 +0200

    improvements

--- lfs.c
+++ lfs.c
@@ -1,19 +1,28 @@
 #include "lfs.h"
 
-int fs_getattr( const char *path, struct stat *stbuf ) {
-	int res = 0;
-	printf("getattr: (path=%s)\n", path);
+inode get_dir_path(const char* path)
+{
 
-	memset(stbuf, 0, sizeof(struct stat));
-	if( strcmp( path, "/" ) == 0 ) {
-		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2;
-	} else if( strcmp( path, "/hello" ) == 0 ) {
-		stbuf->st_mode = S_IFREG | 0777;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = 12;
-	} else
-		res = -ENOENT;
+}
+
+int fs_getattr( const char *path, struct stat *stbuf ) {
+	// int res = 0;
+	// printf("getattr: (path=%s)\n", path);
+	//
+	// memset(stbuf, 0, sizeof(struct stat));
+	// if( strcmp( path, "/" ) == 0 ) {
+	// 	stbuf->st_mode = S_IFDIR | 0755;
+	// 	stbuf->st_nlink = 2;
+	// } else if( strcmp( path, "/hello" ) == 0 ) {
+	// 	stbuf->st_mode = S_IFREG | 0777;
+	// 	stbuf->st_nlink = 1;
+	// 	stbuf->st_size = 12;
+	// } else
+	// 	res = -ENOENT;
+
+	int res = 0;
+	// struct inode inode = get_path_dir(path);
+
 
 	return res;
 }
@@ -39,27 +48,93 @@ int fs_readdir( const char *path, void *buf, fuse_fill_dir_t filler, off_t offse
 }
 
 //Permission
-int fs_open( const char *path, struct fuse_file_info *fi ) {
-    printf("open: (path=%s)\n", path);
+int fs_open( const char *path,
+						 struct fuse_file_info *fi )
+{
+
+  printf("open: (path=%s)\n", path);
 	return 0;
 }
 
-int fs_read( const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi ) {
-    printf("read: (path=%s)\n", path);
-	memcpy( buf, "Hello\n", 6 );
-	return 6;
+int fs_read(const char *path,
+            char *buf,
+            size_t size,
+            off_t offset,
+            struct fuse_file_info *fi )
+{
+  FILE *fp = fopen(DISKNAME, "rb");
+  if(!fp)
+  {
+    //the opening has failed
+  }
+
+  int rp;
+  disk_block *in = malloc(sizeof(disk_block)); //change name
+  while(size > blocksize){
+    if(fread(in, 512, 1, fseek(fp, offset, SEEK_SET)) != 512)//fseek positions the stream
+    {
+      -EAGAIN; //maybew should be something else
+    }
+		memcpy(buf + rp, in->data, 508);
+    size = size - 508;
+    offset = in->next;
+  }
+  //should return num of bytes read (the number requsted on success)
+	return 0;
 }
 
-int fs_write( const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
+void update_free_blocks(free_block_count *first_free, FILE* disk)
+{
+	int nblock = 0;
+	while (&disk+(nblock*512) != NULL)
+	{
+		nblock++;
+	}
+	first_free->first_free_block;
+}
+
+int *get_free_block(free_block_count *first_free, FILE* disk)
+{
+	if (first_free->first_free_block == NULL)
+	{
+		return -10;
+	}
+	int free_block = first_free->first_free_block;
+	free_block->nfree--;
+	if (first_free->nfree == 0) // there are no more blocks
+	{
+		update_free_blocks(first_free, disk);
+	}
+	else
+	{
+		first_free->first_free_block+1; // there are continues blocks left
+	}
+	return free_block;
+}
+
+int fs_write( const char *path,
+							const char *buf,
+							size_t size,
+							off_t offset,
+							struct fuse_file_info *fi)
+{
+	// FILE* fp = fopen(DISKNAME, "w+");
+	//
+	// // disk_block *block_for_file = malloc(sizeof(disk_block));
+	// // memcpy()
+
   return 0;
 }
 
 int fs_release(const char *path, struct fuse_file_info *fi) {
+
 	printf("release: (path=%s)\n", path);
 	return 0;
 }
 
-int fs_mkdir(const char *path , mode_t mode){
+int fs_mkdir(const char *path,
+	 					 mode_t mode)
+{
   return 0;
 }
 int fs_rmdir(const char *path, mode_t mode){
--- lfs.h
+++ lfs.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DISKNAME "file"
+#define BLOCKSIZE 512
+
 int fs_getattr( const char *, struct stat * );
 int fs_readdir( const char *, void *, fuse_fill_dir_t, off_t, struct fuse_file_info * );
 int fs_open( const char *, struct fuse_file_info * );
@@ -25,6 +28,42 @@ static struct fuse_operations lfs_oper = {
 	.read	= fs_read,
 	.release = fs_release, //closes a file
 	.write = fs_write,
-	.rename = fs_rename,
 	.utime = fs_utime,     //maybe should use uitemns (used to update access and modification of file)
+};
+// test/
+//		hej/
+//			file1.c
+//		file.c
+
+// test/
+//		file.c
+//		hej/
+//			file1.c
+
+struct inode_table{
+	inode max_inodes[30];
+	int free_inodes;
+};
+
+struct inode{
+  ino_t inode_no;
+	inode *parent;
+	inode *next_inode;
+  size_t size;
+  time_t atime;     //access time
+  time_t mtime      //modification time
+	disk_block* start;
+	disk_block* end;
+  char *name;
+	int file_type; // 0 if file, 1 if dir
+} ;
+
+struct disk_block{
+	disk_block *next;
+	char* data; // blocksize -4 for the next block pointer
+};
+
+struct free_block_count{
+	int first_free_block; // first free block number
+	int nfree; // free continues blocks
 };
