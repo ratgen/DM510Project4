1f16e1f23aea743a91ed663a68eaf691e3034966
Peter Heilbo Ratgen <peterratgen@gmail.com>
2019-05-14 16:39:07 +0200

    implemented directories

--- lfs.c
+++ lfs.c
@@ -13,6 +13,8 @@ int lfs_open( const char *, struct fuse_file_info * );
 int lfs_read( const char *, char *, size_t, off_t, struct fuse_file_info * );
 int lfs_release(const char *path, struct fuse_file_info *fi);
 int lfs_mkdir(const char* path, mode_t mode);
+int lfs_rmdir(const char* path, mode_t mode);
+
 
 static struct fuse_operations lfs_oper = {
 	.getattr	= lfs_getattr,
@@ -20,7 +22,7 @@ static struct fuse_operations lfs_oper = {
 	.mknod = NULL,
 	.mkdir = lfs_mkdir,
 	.unlink = NULL,
-	.rmdir = NULL,
+	.rmdir = lfs_rmdir,
 	.truncate = NULL,
 	.open	= lfs_open,
 	.read	= lfs_read,
@@ -357,7 +359,35 @@ int lfs_mkdir(const char* path, mode_t mode)
   return 0;
 }
 
-int lfs_readdir( const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi ) {
+int lfs_rmdir(const char* path, mode_t mode)
+{
+  int block = get_block_from_path(path);
+  if(block < 0)
+  {
+    return block;
+  }
+  union lfs_block* rm_block = readblock(block);
+  union lfs_block* rm_block_parent = readblock(rm_block->inode.parent);
+   for(int i = 0; i < 235; i++)
+  {
+    if(rm_block_parent->inode.data[i] == block)
+    {
+      rm_block_parent->inode.data[i] = 0;
+      break;
+    }
+  }
+  writeblock(rm_block_parent,rm_block->inode.parent);
+
+  free_block(rm_block->inode.data[0]);
+  free_block(block);
+  free(rm_block);
+  free(rm_block_parent);
+
+  return 0;
+}
+
+int lfs_readdir( const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi )
+{
 	(void) offset;
 	(void) fi;
   filler(buf, ".", NULL, 0);
